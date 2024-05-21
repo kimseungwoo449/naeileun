c3e79064b049da2d30d00e940d23625e430a83eb
da092511 <da092511@naver.com>
2024-05-21 18:01:58 +0900

    Modify router

--- src/router.js
+++ src/router.js
@@ -2,13 +2,14 @@ import {
     createBrowserRouter,
 } from "react-router-dom";
 import Root from "./components/Root";
+import StudyPage from "./components/StudyPage";
 
 const router = createBrowserRouter([
     
     {
         path: "/",
         element: <Root />,
-        
+
     },
     {
         path: "/user",
@@ -19,7 +20,29 @@ const router = createBrowserRouter([
                 element: <h4>Hello</h4>,
             },
         ]
-    },
+    },{
+        path: "/study",
+        element: <Root />,
+        children:[
+            {
+                path: "",
+                element: <>
+                    <StudyPage />
+                </>
+            },
+        ]
+    },{
+        path: "/study/detail",
+        element: <Root />,
+        children:[
+            {
+                path: "",
+                element: <>
+                    <StudyPage />
+                </>
+            },
+        ]
+    }
 ],{
     basename:"/naeileun",
 });
