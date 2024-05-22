466079fc0a9b86a045e5e93d4bc889e6b4c7ddf1
kimseungwoo449 <ehdanf449@naver.com>
2024-05-22 17:52:39 +0900

    add writeResume

--- src/components/resume/WriteResume.js
+++ src/components/resume/WriteResume.js
@@ -1,13 +1,59 @@
 import { Box, Button, Flex, Grid, Input, Text } from '@chakra-ui/react';
 import React from 'react';
-import { Form } from 'react-router-dom';
+import { Form, useNavigate } from 'react-router-dom';
+
+
 
 const WriteResume = () => {
+    const navigate = useNavigate();
+    
+    const submit = e => {
+        e.preventDefault();
+    
+        const splitValue = "wLYPvSwquc";
+    
+        const req = {
+            "user_id": e.target.id.value,
+            "name": e.target.name.value,
+            "title": e.target.title.value,
+            "user_age": e.target.age.value,
+            "academic_career": e.target.ac1.value + splitValue + e.target.ac2.value + splitValue + e.target.ac3.value,
+            "career": e.target.career1.value + splitValue + e.target.career2.value + splitValue + e.target.career3.value,
+            "skill": e.target.skill1.value + splitValue + e.target.skill2.value + splitValue + e.target.skill3.value,
+            "certificate": e.target.certificate1.value + splitValue + e.target.certificate2.value + splitValue + e.target.certificate3.value,
+            "language": e.target.language1.value + splitValue + e.target.language2.value + splitValue + e.target.language3.value,
+            "award": e.target.award1.value + splitValue + e.target.award2.value + splitValue + e.target.award3.value,
+            "split_value": splitValue
+        }
+    
+    
+    
+        fetch(`${process.env.REACT_APP_SERVER_URL}/resume`, {
+            method: 'POST',
+            headers: {
+                Authorization: `ADMIN ${process.env.REACT_APP_ADMIN_KEY}`,
+                "Content-Type": "application/json;charset=UTF8"
+            },
+            body: JSON.stringify(req),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+
+                if(data.status){
+                    navigate('/');
+                }else{
+                    navigate('/resume/write');
+                }
+            });
+    }
+
+
     return (
         <Box w={'70%'} m={'auto'}>
-            <Form action={`${process.env.REACT_APP_SERVER_URL}/user/resume/writeAction`}>
+            <Form method='POST' action={`${process.env.REACT_APP_SERVER_URL}/resume`} onSubmit={submit}>
                 <Flex flexDir={'column'}>
-                <Text as='b' fontSize={'2xl'} textAlign={'center'} mb={'20px'}>이력서 작성</Text>
+                    <Text as='b' fontSize={'2xl'} textAlign={'center'} mb={'20px'}>이력서 작성</Text>
                     <Box id='user_info-container' mb={'10px'} >
                         <Input type='text' name='title' id='resume-title' placeholder='이력서 제목' mb={'10px'} />
                         <Input type='text' name='name' id='user-name' placeholder='이름' mb={'10px'} />
@@ -21,11 +67,11 @@ const WriteResume = () => {
                     </Box>
                     <Box id='academic-career-container' mb={'10px'} >
                         <Text as='b' fontSize={'xl'}>학력</Text>
-                        <Input type='text' name='academic-career1' id='resume-academic-career1' placeholder='학력1' m={'10px 0 10px 0'} />
-                        <Input type='text' name='academic-career2' id='resume-academic-career2' placeholder='학력2' mb={'10px'} />
-                        <Input type='text' name='academic-career3' id='resume-academic-career3' placeholder='학력3' />
+                        <Input type='text' name='ac1' id='resume-academic-career1' placeholder='학력1' m={'10px 0 10px 0'} />
+                        <Input type='text' name='ac2' id='resume-academic-career2' placeholder='학력2' mb={'10px'} />
+                        <Input type='text' name='ac3' id='resume-academic-career3' placeholder='학력3' />
                     </Box>
-                    <Box  display={'flex'} mb={'10px'} >
+                    <Box display={'flex'} mb={'10px'} >
                         <Box id='certificate-container' mr={'20px'} w={'50%'} >
                             <Text as='b' fontSize={'xl'} >자격증</Text>
                             <Input type='text' name='certificate1' id='resume-certificate1' placeholder='자격증1' m={'10px 0 10px 0'} />
@@ -34,9 +80,9 @@ const WriteResume = () => {
                         </Box>
                         <Box id='language-container' w={'50%'} >
                             <Text as='b' fontSize={'xl'} >언어</Text>
-                            <Input type='text' name='lanquage1' id='resume-lanquage1' placeholder='언어1' m={'10px 0 10px 0'} />
-                            <Input type='text' name='lanquage2' id='resume-lanquage2' placeholder='언어2' mb={'10px'} />
-                            <Input type='text' name='lanquage3' id='resume-lanquage3' placeholder='언어3' />
+                            <Input type='text' name='language1' id='resume-language1' placeholder='언어1' m={'10px 0 10px 0'} />
+                            <Input type='text' name='language2' id='resume-language2' placeholder='언어2' mb={'10px'} />
+                            <Input type='text' name='language3' id='resume-language3' placeholder='언어3' />
                         </Box>
                     </Box>
                     <Box display={'flex'} mb={'10px'}>
@@ -50,15 +96,15 @@ const WriteResume = () => {
                             <Text as='b' fontSize={'xl'} >수상이력</Text>
                             <Input type='text' name='award1' id='resume-award1' placeholder='수상이력1' m={'10px 0 10px 0'} />
                             <Input type='text' name='award2' id='resume-award2' placeholder='수상이력2' mb={'10px'} />
-                            <Input type='text' name='award3' id='resume-award3' placeholder='수상이력3'/>
+                            <Input type='text' name='award3' id='resume-award3' placeholder='수상이력3' />
                         </Box>
                     </Box>
                     <Box alignSelf={'center'} w={'50%'} display={'grid'} gridTemplateColumns={'30% 30%'} justifyContent={'space-around'}>
-                        <Input _hover={{cursor:'pointer'}} type='submit' value={'저장하기'} backgroundColor={'black'} color={'white'} />
+                        <Input _hover={{ cursor: 'pointer' }} type='submit' value={'저장하기'} backgroundColor={'black'} color={'white'} />
                         <Button backgroundColor={'red'} color={'white'} >취소하기</Button>
                     </Box>
                 </Flex>
-                <Input type='hidden' name='command' value='write' />
+                <Input type='hidden' name='id' value={'djdjdjd'} />
             </Form>
         </Box>
     );
