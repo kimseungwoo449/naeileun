ea5660f76234eaa5d0e14719910827931d657278
da092511 <da092511@naver.com>
2024-05-23 17:31:27 +0900

    Create StudyCreate page

---
+++ src/components/study/StudyCreate.js
@@ -0,0 +1,25 @@
+import {React} from 'react';
+import {Box} from '@chakra-ui/react';
+
+
+
+const StudyCreate = () =>{
+
+
+
+
+
+
+
+
+    return(
+        <>
+        <Box h={'80vh'} >
+
+
+        </Box>
+        </>
+    )
+}
+
+export default StudyCreate;
