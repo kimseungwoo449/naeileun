995f120967500f5837309085794af1bfee1add11
da092511 <da092511@naver.com>
2024-05-23 18:03:48 +0900

    Modify return

--- src/components/study/StudyCreate.js
+++ src/components/study/StudyCreate.js
@@ -1,5 +1,6 @@
 import {React} from 'react';
-import {Box} from '@chakra-ui/react';
+
+import {Box, HStack,Input,Text,Textarea} from '@chakra-ui/react';
 
 
 
@@ -8,15 +9,22 @@ const StudyCreate = () =>{
 
 
 
-
-
-
-
     return(
         <>
-        <Box h={'80vh'} >
+        <Box h={'80vh'} w={'600px'} m={"auto"}  mt={"100px"}>
+            <form method="POST" action="/study/create">
+                <HStack>
+                    <Text as="h2" fontSize={"2xl"} margin={"auto"} mb={"30px"}>스터디 생성하기</Text>
+                </HStack>
+                
+                <HStack>
+                    <Input w={"400px"} m={"auto"} placeholder="스터디 그룹 이름" mb={"20px"}></Input>
+                </HStack>
+                <HStack>
+                    <Textarea h={"250px"} w={"400px"} m={"auto"} placeholder="스터디 그룹 설명" mb={"30px"}></Textarea>
+                </HStack>
 
-
+            </form>
         </Box>
         </>
     )
