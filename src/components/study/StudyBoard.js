9106f29be75768518c1d6f7be7c8e52a323f74a9
da092511 <da092511@naver.com>
2024-05-26 00:23:09 +0900

    Modify return tags

--- src/components/study/StudyBoard.js
+++ src/components/study/StudyBoard.js
@@ -1,6 +1,8 @@
 import {React} from 'react';
-import {Box} from '@chakra-ui/react';
+import {Box, Text, Button, HStack, Image, Icon, Stack} from '@chakra-ui/react';
 import {useNavigate, useLocation } from 'react-router-dom';
+import { IoChatbubble } from "react-icons/io5";
+import { MdSettings } from "react-icons/md";
 
 const StudyBoard = () =>{
 
@@ -30,9 +32,31 @@ const StudyBoard = () =>{
 
     return(
         <>
-        <Box h={'80vh'} >
-        
-
+        <Box h={'80vh'} w={"650px"} ml={'25%'}>
+            <form method="POST" action="">
+                <HStack>
+                    <Text>스터디 그룹명</Text>
+                    <Text>그룹 채팅방 입장</Text>
+                    <Icon as={IoChatbubble}></Icon>
+                    <Button onClick={""} w={'60px'} id="cancle">탈퇴</Button>
+                    <Button onClick={""} w={'60px'} colorScheme='blue'>가입신청</Button>
+                </HStack>
+                <HStack alignItems={'flex-start'}>
+                    <Image src='' border={"solid 1px gray"} w={'100px'} h={'100px'}></Image>
+                    <Stack alignItems={'flex-end'}>
+                        <Text >스터디 그룹 소개</Text>
+                        <Icon as={MdSettings}></Icon>
+                    </Stack>
+                </HStack>
+                <Stack>
+                    <Text>그룹 게시판</Text>
+                    <HStack>
+                        <Text>게시글 제목</Text>
+                        <Text>작성시간</Text>
+                    </HStack>
+                </Stack>
+                
+            </form>
         </Box>
         </>
     )
