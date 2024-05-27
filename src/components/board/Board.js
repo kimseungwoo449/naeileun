5d3a87b34ff3a5b9aa26039fd07982f0148cc3e9
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-27 17:18:44 +0900

    Update : Board component 디자인 수정 및 navigate 변수, 오류 수정

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,4 +1,4 @@
-import { Box, Button, HStack, Heading, Input, Menu, MenuButton, MenuItem, MenuList, Table, TableContainer, Tbody, Td, Tfoot, Th, Thead, Tr, useColorModeValue } from '@chakra-ui/react';
+import { Box, Button, Flex, HStack, Heading, Input, Menu, MenuButton, MenuItem, MenuList, Stack, Text, useColorModeValue } from '@chakra-ui/react';
 import React, { useEffect, useRef, useState } from 'react';
 import { RiArrowDownSLine } from 'react-icons/ri';
 import { TiArrowUnsorted } from 'react-icons/ti';
@@ -9,21 +9,34 @@ const Board = () => {
 
     const [postList, setPostList] = useState([]);
     const [page, setPage] = useState(1);
-    const [search, setSearch] = useState(' ');
+    const [search, setSearch] = useState('');
 
     const pageCount = useRef(1);
 
     const buttonScheme = useColorModeValue("blackAlpha", "whiteAlpha");
 
-    const {state} = useLocation();
-    const {code} = state;
+    const location = useLocation();
+    const code = location.state.code;
+    const boardList = location.state.board;
+    console.log("code: " + code)
+    console.log("boardList: " + boardList)
     
-    const fetchPosts = async() => {
-        console.log('fetchPosts');
-        console.log('${process.env.REACT_APP_SERVER_URL}:', process.env.REACT_APP_SERVER_URL);
-        console.log('Authorization:', process.env.REACT_APP_ADMIN_KEY);
+    const movePage = (e) => {
+        const command = e.target.id;
+        const bordCode = code;
+        const postCode = e.target.getAttribute("name");
+        const board = boardList;
+
+        console.log("board : " + board);
+        console.log("postCode : " + postCode);
+        console.log("command : " + command);
+
+        if (command === 'board-detail')
+            navigate('/board/detail', { state: { bordCode: bordCode, postCode: postCode, board: board } } );
+    }
+
+    const fetchPosts = async () => {
         const url = `${process.env.REACT_APP_SERVER_URL}/board/view/${code}`;
-        console.log('Fetching posts from:', url);
         const response = await fetch(
             url,
             {
@@ -35,76 +48,51 @@ const Board = () => {
         );
 
         const data = await response.json();
-        console.log(data);
 
-        if(data.status){
+        if (data.status) {
             navigate('/board');
         } else {
-            pageCount.current = data.meta.pageable_count % 10 > 0 ? data.meta.pageable_count / 10 + 1 : data.meta.pageable_count / 10;
-            pageCount.current = Math.floor(pageCount.current);
-            pageCount.current = pageCount.current > 15 ? 15 : pageCount.current;
-    
+            pageCount.current = Math.ceil(data.meta.pageable_count / 10);
+            pageCount.current = Math.min(pageCount.current, 15);
+
             setPostList(data.result);
         }
-
     }
-    
+
     const changeSearch = e => {
-        if(e.target.value.length >= 2)
-            setSearch(e.target.value);
+        setSearch(e.target.value);
     }
 
     useEffect(() => {
-        console.log('useEffect');
         fetchPosts();
     }, [page, search]);
 
     return (
-        <>
-            <Box>
-                <Heading size='xl' margin={'30px'}>게시판 명</Heading>
-                    <Menu>
-                        <MenuButton as={Button} rightIcon={<RiArrowDownSLine />}>
-                            게시판 명
-                        </MenuButton>
-                        <MenuList>
-                            {postList.map((post, index) => (
-                                <MenuItem>{post.boardName}</MenuItem>
-                            ))}
-                        </MenuList>
-                    </Menu>
-            </Box>
-            <Box margin={'auto'}>
-                <TableContainer w={"1000px"}>
-                    <Table  m={"40px 0"}>
-                        <Thead>
-                            <Tr>
-                                <Th>번호</Th>
-                                <Th>제목</Th>
-                                <Th>작성자</Th>
-                                <Th>작성일</Th>
-                                <Th>추천수</Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            {postList.map((post, index) => (
-                                <>
-                                    <Tr>
-                                        <Td>{(page - 1) * 10 + index + 1}</Td>
-                                        <Td>{post.title}</Td>
-                                        <Td>{post.userId}</Td>
-                                        <Td>{post.writeDate}</Td>
-                                        <Td>{post.recommandation}</Td>
-                                    </Tr>
-                                </>
-                            ))}
-                        </Tbody>
-                        <Tfoot>
-
-                        </Tfoot>
-                    </Table>
-                </TableContainer>
-                <Input type="text" placeholder="검색어 입력" size="lg" w={"600px"} />
+        // Flex 컨테이너를 사용하여 전체 레이아웃을 세로로 정렬합니다.
+        <Flex minW={'900px'} ml={'150px'} margin={"auto"} direction="column">
+            {/* 상단 헤더 및 검색창 */}
+            <Box p="6">
+                {boardList.map((board, index) => (
+                    <Heading size='lg' mb="4">{board.boardCode == code ? board.boardName : ""}</Heading>
+                ))}
+                <Menu>
+                    <MenuButton as={Button} rightIcon={<RiArrowDownSLine />}>
+                        게시판 선택
+                    </MenuButton>
+                    <MenuList>
+                        {boardList.map((board, index) => (
+                            <MenuItem key={index}>{board.boardName}</MenuItem>
+                        ))}
+                    </MenuList>
+                </Menu>
+                <Input 
+                    placeholder="검색어 입력" 
+                    size="md" 
+                    w={"300px"} 
+                    mt="4" 
+                    value={search}
+                    onChange={changeSearch}
+                />
                 <Menu>
                     <MenuButton as={Button} rightIcon={<TiArrowUnsorted />}>
                         통합 검색
@@ -114,22 +102,39 @@ const Board = () => {
                         <MenuItem>작성자 아이디</MenuItem>
                     </MenuList>
                 </Menu>
-                <HStack mb={"40px"} justifyContent={"center"} wrap={"wrap"} margin={'50px'}>
+            </Box>
+
+            {/* 게시글 목록 */}
+            <Box p="6" minHeight="70vh">
+                {postList.map((post, index) => (
+                    <Box onClick={movePage} id = "board-detail"  name={post.postCode} key={index} p="4" borderWidth="1px" borderRadius="md" mb="4" _hover={{ cursor: "pointer" }}>
+                        <Text id = "board-detail"  name={post.postCode} fontSize='xl' fontWeight={"bold"}>{post.title}</Text>
+                        <Text id = "board-detail"  name={post.postCode}>{post.content}</Text>
+                        <HStack id = "board-detail"  name={post.postCode} spacing="4" mt="2">
+                            <Text id = "board-detail"  name={post.postCode}>작성자 {post.userId}</Text>
+                            <Text id = "board-detail"  name={post.postCode}>추천 {post.recommandation}</Text>
+                            <Text id = "board-detail"  name={post.postCode}>댓글 {post.comments}</Text>
+                            <Text id = "board-detail"  name={post.postCode}>작성일자 {post.writeDate}</Text>
+                        </HStack>
+                    </Box>
+                ))}
+            </Box>
+
+            {/* 페이지 네비게이션 */}
+            <Box mt="6" mb="4" textAlign="center">
+                <HStack spacing="4" justify="center">
                     {Array.from({ length: pageCount.current }, (_, index) => (
-                        <>
                         <Button
+                            key={index}
                             colorScheme={page === index + 1 ? "gray" : buttonScheme}
-                            onClick={(e) => {
-                            setPage(index + 1);
-                            }}
+                            onClick={() => setPage(index + 1)}
                         >
                             {index + 1}
                         </Button>
-                        </>
                     ))}
                 </HStack>
             </Box>
-        </>
+        </Flex>
     );
 };
 
