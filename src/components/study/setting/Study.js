decf4b0345bcd27b25698beaae6de8fcf93297ae
da092511 <da092511@naver.com>
2024-05-28 17:46:17 +0900

    Create Study setting access study

---
+++ src/components/study/setting/Study.js
@@ -0,0 +1,58 @@
+import {React, useState, useEffect} from 'react';
+import { Heading, Box, HStack,Card,Image,Stack,CardBody,Text,Icon,Button} from '@chakra-ui/react';
+import {useNavigate, useLocation } from 'react-router-dom';
+const Study= () =>{
+    const navigate = useNavigate();
+    const location = useLocation();
+    const groupCode = location.state.groupCode;
+    const adminCode = location.state.adminCode;
+
+    const fetchDelete = async() =>{
+        console.log(groupCode);
+
+        const req ={
+            "group_code" : groupCode
+        }
+
+        const response = await fetch(
+            `${process.env.REACT_APP_SERVER_URL}/study/delete`,
+            {
+                method : "POST",
+                headers: {
+                    Authorization: `ADMIN ${process.env.REACT_APP_ADMIN_KEY}`,
+                    "Content-Type": "application/json;charset=UTF8"
+                },
+                body:JSON.stringify(req)
+            }
+        )
+
+        const data = await response.json();
+        console.log(data.state);
+
+        if(data.state === true){
+            console.log('here');
+            navigate('/study');
+        }
+        
+    }
+
+    const studyDelete=()=>{
+        fetchDelete();
+    }
+
+    return(
+        <>
+            <Stack mt={'15px'} ml={'45px'}>
+                <Text as={'b'} fontSize={'1.2em'} >멤버 승인</Text>
+                <HStack>
+                    <Text w={'140px'}>유저 아이디</Text>
+                    <Text w={'220px'}>가입신청 내용</Text>
+                    <Button h={'35px'} fontSize={'0.9em'} colorScheme='red' onClick={studyDelete}>스터디 삭제</Button>
+                    <Button h={'35px'} fontSize={'0.9em'} colorScheme='blue'>승인</Button>
+                </HStack>
+            </Stack>
+        </>
+    )
+}
+
+export default Study;
