9ad6aeefebd4bbbb1e0202699b4611ed22e76830
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-28 17:44:43 +0900

    Add : CreatePost component submit 함수 추가

--- src/components/board/post/CreatePost.js
+++ src/components/board/post/CreatePost.js
@@ -3,9 +3,11 @@ import { Box, Button, FormControl, Input, Textarea, VStack, HStack, IconButton, 
 import { FaRegSmile } from 'react-icons/fa';
 import { BsImage } from 'react-icons/bs';
 import { AiOutlineLink } from 'react-icons/ai';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Form, useLocation, useNavigate } from 'react-router-dom';
 
 const CreatePost = () => {
+    const navigate = useNavigate();
+
     const location = useLocation();
     const boardCode = location.state.boardCode;
     const boardList = location.state.board;
@@ -15,7 +17,43 @@ const CreatePost = () => {
     const TITLE_MAX_LENGTH = 50;
     const CONTENT_MAX_LENGTH = 4000;
 
+    const userStr = sessionStorage.getItem('user');
+    const user = JSON.parse(userStr);
+
     let [inputCount, setInputCount] = useState(0);
+
+    const submit = e => {
+        e.preventDefault();
+    
+        const req = {
+            "title": e.target.reqTitle.value,
+            "content": e.target.reqContent.value,
+            "user_id": user.id,
+            "board_code": e.target.reqBoardCode.value
+        }
+        console.log("req : "+req);
+    
+        fetch(`${process.env.REACT_APP_SERVER_URL}/board/write`, {
+            method: 'POST',
+            headers: {
+                Authorization: `ADMIN ${process.env.REACT_APP_ADMIN_KEY}`,
+                "Content-Type": "application/json;charset=UTF8"
+            },
+            body: JSON.stringify(req),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+
+                if(data.status){
+                    navigate('/');
+                }else{
+                    const resBoardCode = data.result.boardCode;
+                    const postCode = data.result.postCode;
+                    navigate('/board/detail', { state: { boardCode: resBoardCode, postCode: postCode, boardList: boardList } });
+                }
+            });
+    }
 
     const onInputHandler = (e) => {
         console.log("id : "+e.target.id)
@@ -37,36 +75,37 @@ const CreatePost = () => {
                 <Box textAlign="center" mb={5} mt={5}>
                     <Heading as="h3" size="lg">게시글 작성</Heading>
                 </Box>
-                <VStack spacing={4} align="stretch">
-                    <Select variant='flushed'>
-                        <option value='option' disabled>게시판 선택</option>
-                        {boardList.map((board, index) => (
-                            <option id='board-code' value={board.boardCode} selected={board.boardCode === boardCode}>{board.boardName}</option>
-                        ))}
-                    </Select>
-                    <FormControl id="title">
-                        <Input id='title' minHeight="5vh" type="text" placeholder="제목" onChange={onInputHandler} maxLength={TITLE_MAX_LENGTH} wordBreak={"break-all"} />
-                        <Box textAlign={"end"}>
-                            <span>{inputCount}</span> {/* 글자수 표시하는 부분 */}
-                            <span>/{TITLE_MAX_LENGTH} 자</span>
-                        </Box>
-                    </FormControl>
-                    <FormControl id="content">
-                        <Textarea id='content' minHeight="30vh" placeholder="내용" onChange={onInputHandler} maxLength={CONTENT_MAX_LENGTH} wordBreak={"break-all"}  />
-                        <Box textAlign={"end"}>
-                            <span>{inputCount}</span> {/* 글자수 표시하는 부분 */}
-                            <span>/{CONTENT_MAX_LENGTH} 자</span>
-                        </Box>
-                    </FormControl>
-                    <HStack justifyContent="space-between">
-                        <HStack>
-                            <IconButton icon={<FaRegSmile />} aria-label="Emoji" />
-                            <IconButton icon={<BsImage />} aria-label="Image" />
-                            <IconButton icon={<AiOutlineLink />} aria-label="Link" />
+                <Form method='POST' action={`${process.env.REACT_APP_SERVER_URL}/board/write`} onSubmit={submit}>
+                    <VStack spacing={4} align="stretch">
+                        <Select variant='flushed'>
+                            <option value='option' disabled>게시판 선택</option>
+                            {boardList.map((board, index) => (
+                                <option id='board-code' name='reqBoardCode' value={board.boardCode} selected={board.boardCode == boardCode}>{board.boardName}</option>
+                            ))}
+                        </Select>
+                        <FormControl id="title">
+                            <Input id='title' name='reqTitle' minHeight="5vh" type="text" placeholder="제목" onChange={onInputHandler} maxLength={TITLE_MAX_LENGTH} />
+                            <Box textAlign={"end"}>
+                                <span>{inputCount}</span> {/* 글자수 표시하는 부분 */}
+                                <span>/{TITLE_MAX_LENGTH} 자</span>
+                            </Box>
+                        </FormControl>
+                        <FormControl id="content">
+                            <Textarea id='content' name='reqContent' minHeight="50vh" placeholder="내용" onChange={onInputHandler} maxLength={CONTENT_MAX_LENGTH} wordBreak={"break-all"}  />
+                            <Box textAlign={"end"}>
+                                <span>{inputCount}</span> {/* 글자수 표시하는 부분 */}
+                                <span>/{CONTENT_MAX_LENGTH} 자</span>
+                            </Box>
+                        </FormControl>
+                        <HStack justifyContent="space-between">
+                            <HStack>
+                                <IconButton icon={<FaRegSmile />} aria-label="Emoji" />
+                                <IconButton icon={<BsImage />} aria-label="Image" />
+                            </HStack>
+                            <Button type='submit' colorScheme="gray" size="md">등록</Button>
                         </HStack>
-                        <Button colorScheme="gray" size="md">등록</Button>
-                    </HStack>
-                </VStack>
+                    </VStack>
+                </Form>
             </Box>
         </>
     );
