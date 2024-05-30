a2d2ec5ec2f443c304b3dd60305406402d250b18
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-30 17:22:30 +0900

    Update : UpdatePost component 기존 글 가져오도록 변경

--- src/components/board/post/UpdatePost.js
+++ src/components/board/post/UpdatePost.js
@@ -1,30 +1,67 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Box, Button, FormControl, Input, Textarea, VStack, HStack, IconButton, Heading, Select, Image } from '@chakra-ui/react';
 import { FaRegSmile } from 'react-icons/fa';
 import { BsImage } from 'react-icons/bs';
 import { Form, useLocation, useNavigate } from 'react-router-dom';
 import Picker from 'emoji-picker-react';
+import { useLogin } from '../../LoginContext';
 
 const UpdatePost = () => {
     const navigate = useNavigate(); // 페이지 이동을 위한 훅
     const location = useLocation(); // 현재 위치 정보를 가져오기 위한 훅
-    const boardCode = location.state.boardCode; // 전달받은 boardCode
+    const boardCode = location.state.code; // 전달받은 boardCode
+    const postCode = location.state.postCode; // 전달받은 postCode
     const boardList = location.state.board; // 전달받은 board 리스트
+    console.log("postCode: " + postCode);
+    console.log("boardCode: " + boardCode);
+    console.log("boardList: " + boardList);
 
     const TITLE_MAX_LENGTH = 50; // 제목 최대 글자수
     const CONTENT_MAX_LENGTH = 4000; // 내용 최대 글자수
 
-     const userStr = sessionStorage.getItem('user'); // 세션 스토리지에서 사용자 정보 가져오기
-    const user = JSON.parse(userStr); // JSON 문자열을 객체로 변환
-
-    const [title, setTitle] = useState(''); // 제목 상태 추가
-    const [content, setContent] = useState('');
-    const [inputCount, setInputCount] = useState(0); // 입력된 글자수 상태 관리
-    const [selectedFile, setSelectedFile] = useState(null); // 선택된 파일 상태 관리
-    const [preview, setPreview] = useState(null); // 파일 미리보기 URL 상태 관리
-    const [showEmojiPicker, setShowEmojiPicker] = useState(false); // 이모티콘 선택기 표시 상태 관리
+    const [post, setPost] = useState({}); // post 상태 초기값을 빈 객체로 설정
+    const [title, setTitle] = useState(''); // 제목 상태
+    const [content, setContent] = useState(''); // 내용 상태
+    const [inputCount, setInputCount] = useState(0); // 입력된 글자수 상태
+    const [selectedFile, setSelectedFile] = useState(null); // 선택된 파일 상태
+    const [preview, setPreview] = useState(null); // 파일 미리보기 URL 상태
+    const [showEmojiPicker, setShowEmojiPicker] = useState(false); // 이모티콘 선택기 표시 상태
+    const { user } = useLogin(); // 로그인 상태를 가져오기 위한 훅
 
     const fileInputRef = useRef(null); // 파일 입력 요소에 접근하기 위한 ref
+
+    // 게시글 데이터를 가져오는 함수
+    const fetchPost = async () => {
+        const url = `${process.env.REACT_APP_SERVER_URL}/board/view/${boardCode}/${postCode}`;
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                Authorization: `ADMIN ${process.env.REACT_APP_ADMIN_KEY}`
+            },
+        });
+
+        const data = await response.json();
+        console.log(data);
+
+        if (data.status) {
+            navigate('/board'); // 게시글을 찾지 못한 경우 게시판 페이지로 이동
+        } else {
+            setPost(data.result); // 게시글 데이터를 상태로 설정
+        }
+    };
+
+    // 컴포넌트가 마운트될 때 게시글 데이터를 가져옴
+    useEffect(() => {
+        fetchPost();
+    }, []); // 빈 의존성 배열 추가하여 컴포넌트가 마운트될 때만 실행되도록 설정
+
+    // post 상태가 업데이트될 때 title과 content 상태를 업데이트
+    useEffect(() => {
+        if (post.title) {
+            setTitle(post.title); // 제목 상태 설정
+            setContent(post.content); // 내용 상태 설정
+        }
+    }, [post]); // post 상태가 업데이트될 때 실행
 
     // 폼 제출 핸들러
     const submit = e => {
@@ -61,7 +98,7 @@ const UpdatePost = () => {
                     navigate('/board/detail', { state: { boardCode: resBoardCode, postCode: postCode, boardList: boardList } }); // 게시글 상세 페이지로 이동
                 }
             });
-    }
+    };
 
     // 입력 핸들러
     const onInputHandler = (e) => {
@@ -76,10 +113,10 @@ const UpdatePost = () => {
         if (id === 'title') {
             setTitle(value); // 제목 상태 업데이트
         } else if (id === 'content') {
-            setContent(value);
+            setContent(value); // 내용 상태 업데이트
         }
 
-        setInputCount(value.length);// 입력된 글자수 상
+        setInputCount(value.length); // 입력된 글자수 상태 업데이트
     };
 
     // 파일 선택 핸들러
@@ -115,15 +152,16 @@ const UpdatePost = () => {
                 <Form method='POST' action={`${process.env.REACT_APP_SERVER_URL}/board/write`} onSubmit={submit} encType='multipart/form-data'>
                     <VStack spacing={4} align="stretch">
                         {/* 게시판 선택 */}
-                        <Select variant='flushed'>
-                            <option value='option' disabled>게시판 선택</option>
+                        <Select variant='flushed' value={boardCode} disabled>
                             {boardList.map((board, index) => (
-                                <option key={index} value={board.boardCode} selected={board.boardCode === boardCode}>{board.boardName}</option>
+                                <option key={index} value={board.boardCode}>
+                                    {board.boardName}
+                                </option>
                             ))}
                         </Select>
                         {/* 제목 입력 */}
                         <FormControl id="title">
-                            <Input id='title' name='reqTitle' minHeight="5vh" type="text" placeholder="제목" onChange={onInputHandler} maxLength={TITLE_MAX_LENGTH} />
+                            <Input id='title' name='reqTitle' minHeight="5vh" type="text" onChange={onInputHandler} maxLength={TITLE_MAX_LENGTH} value={title} />
                             <Box textAlign={"end"}>
                                 <span>{title.length}</span>
                                 {/* 글자수 표시 */}
@@ -136,7 +174,6 @@ const UpdatePost = () => {
                                 id='content'
                                 name='reqContent'
                                 minHeight="50vh"
-                                placeholder="내용"
                                 value={content}
                                 onChange={onInputHandler}
                                 maxLength={CONTENT_MAX_LENGTH}
@@ -154,7 +191,6 @@ const UpdatePost = () => {
                                 <Image src={preview} alt="Selected File" maxHeight="200px" mb={4} />
                             </Box>
                         )}
-
                         {/* 이모티콘 및 파일 업로드 아이콘 */}
                         <HStack justifyContent="space-between">
                             <HStack>
@@ -171,17 +207,17 @@ const UpdatePost = () => {
                                 <IconButton
                                     icon={<BsImage />}
                                     aria-label="Image"
-                                    onClick={() => fileInputRef.current.click()} // 아이콘 클릭 시 파일 입력 창 열기
+                                    onClick={() => fileInputRef.current.click()} // 아이콘 클릭 시 파일 선택 창 열기
                                 />
                                 <input
                                     type="file"
                                     ref={fileInputRef}
                                     style={{ display: 'none' }}
                                     onChange={handleFileChange}
-                                    accept="image/*" // 이미지 파일만 선택 가능
+                                    accept="image/*"
                                 />
                             </HStack>
-                            <Button type='submit' colorScheme="gray" size="md">등록</Button>
+                            <Button type='submit' colorScheme="gray" size="md">수정</Button>
                         </HStack>
                     </VStack>
                 </Form>
